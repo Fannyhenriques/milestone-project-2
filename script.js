b594333df092dd95ec61d83830e17f7efea05bb4
iamfannyhenriques <fanny.henriques@hotmail.com>
2026-03-04 18:25:26 +0100

    Set up DOM element references in script.js and add hidden class in style.css

---
+++ script.js
@@ -0,0 +1,10 @@
+// getting the elements
+const startForm = document.getElementById("start-form");
+const startSection = document.getElementById("start-section");
+const gameBoard = document.querySelector(".game-board");
+const gameInfo = document.querySelector(".game-info");
+const welcomeMessage = document.getElementById("welcome-message");
+
+const movesEl = document.getElementById("moves");
+const missesEl = document.getElementById("misses");
+const scoreEl = document.getElementById("score");
